14d90ed6d27db131b85c20028e03acf3f7710d85
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-07-10 15:38:23 +0530

    Initialize Backend

---
+++ backend/src/test/java/com/aeroquell/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeroquell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
